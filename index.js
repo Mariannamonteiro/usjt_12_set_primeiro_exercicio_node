2fa89dcca56e9171056df1225f0da2599fd8b032
Marianna Monteiro de Oliveira - 820120673 <820120673@ulife.com.br>
2022-09-12 20:38:46 -0300

    feat: new endpoint get users

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
+const usuarios = {}
+
+app.get('/rede/usuarios',(req, res) => {
+    res.status(200).send(usuarios)
+})
+
 
 app.listen(5000, () => console.log('Usuarios. Porta 5000'))
