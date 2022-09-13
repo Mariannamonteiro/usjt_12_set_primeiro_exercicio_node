43a8c405bac75277ce70d2115c156611adeb97a1
Marianna Monteiro de Oliveira - 820120673 <820120673@ulife.com.br>
2022-09-12 21:30:29 -0300

    feat: method to create a new user

--- index.js
+++ index.js
@@ -2,11 +2,25 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
-const usuarios = {}
+const usuarios = []
+let userId = 0;
 
-app.get('/rede/usuarios',(req, res) => {
+//get all users endpoint
+app.get('/rede/usuarios', (req, res) => {
     res.status(200).send(usuarios)
 })
 
+
+app.post('/rede/usuarios', (req, res) => {
+    userId++
+    usuarios.forEach(user => {
+        if (req.body.email === user.email) {
+            return res.send('E-mail já cadastrado na base')
+
+        }
+    });
+    usuarios.push({id:userId,nome:req.body.nome, email:req.body.email})
+    res.send('Usuário cadastrado com sucesso').status(201)
+})
 
 app.listen(5000, () => console.log('Usuarios. Porta 5000'))
